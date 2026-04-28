e89cc56abece2f60b2a903d216c7afabfe8573ca
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-04-27 20:41:36 -0400

    Agregando microservicio de profesores

---
+++ servicio-profesores/src/main/java/com/duoc/servicio_profesores/ServicioProfesoresApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.servicio_profesores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioProfesoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioProfesoresApplication.class, args);
+	}
+
+}
---
+++ servicio-profesores/src/test/java/com/duoc/servicio_profesores/ServicioProfesoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.servicio_profesores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioProfesoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
