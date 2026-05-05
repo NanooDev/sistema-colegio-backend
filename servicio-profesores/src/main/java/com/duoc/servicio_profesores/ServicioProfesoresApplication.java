750cf12b183018ca6aedf5525f94fa025a745081
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-04 21:11:39 -0400

    Cambios Alvaro

--- servicio-profesores/src/main/java/com/duoc/servicio_profesores/ServicioProfesoresApplication.java
+++ servicio-profesores/src/main/java/com/duoc/servicio_profesores/ServicioProfesoresApplication.java
@@ -2,8 +2,10 @@ package com.duoc.servicio_profesores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ServicioProfesoresApplication {
 
 	public static void main(String[] args) {
