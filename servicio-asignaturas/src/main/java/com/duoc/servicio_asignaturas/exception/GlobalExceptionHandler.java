048d97d306faf77386b877a5886aa3f26b7cd518
NanooDev <marianoacevedo1809@gmail.com>
2026-06-15 21:49:07 -0400

    Refactor: Alineación total con el estilo de Docker Compose del docente (redes, healthchecks) y corrección de imports de Slf4j

--- servicio-asignaturas/src/main/java/com/duoc/servicio_asignaturas/exception/GlobalExceptionHandler.java
+++ servicio-asignaturas/src/main/java/com/duoc/servicio_asignaturas/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.duoc.servicio_asignaturas.exception;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
